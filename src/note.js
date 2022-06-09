dfbe18e99d1b4d10e98958c4cb2377ced8b58fb2
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-06-09 14:32:04 +0600

    first commit

---
+++ src/note.js
@@ -0,0 +1,12 @@
+/* 
+1. add resume button. it will appear without scrolling the page. it will download automatically or will open in a new tab.
+2. there will be small description and name of your project (3-6). details will be in another page. 
+3. home page will be responsive 
+4. there will be a contract me button in home page in menu bar or at the bottom.
+5. This contact form can be in separate page or modal or in the same page.
+6. When clicking on submit button, a email will be send to my email address.
+7. no need to work in backend for this email sending system. just search for free contact form in google.
+8. Post your website content to facebook page. 
+9. Post your live site link to programming hero. 
+10. 
+ */
