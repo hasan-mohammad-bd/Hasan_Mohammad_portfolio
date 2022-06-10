3e3ddc1536b5013f4584be48900c3fcba9986f1e
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-06-10 23:48:31 +0600

    adding component

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Bio from '../Bio';
 import Banner from './Banner';
+import Skill from './Skill';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Bio></Bio>
+            <hr />
+            <Skill></Skill>
         </div>
     );
 };
---
+++ src/component/Home/Skill.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Skill = () => {
+    return (
+        <div>
+            <h2 className='text-center my-5'>My Advantage</h2>
+            <p></p>
+        </div>
+    );
+};
+
+export default Skill;
