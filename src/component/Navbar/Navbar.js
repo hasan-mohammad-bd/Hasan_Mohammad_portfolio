f82188bd41cf7530f27ab4cf0b8dff0d48f9e847
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-06-10 11:35:51 +0600

    adding navbar

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../img/hasan-logo.png'
 
 const Navbar = () => {
     return (
-        <div className='bg-red-400'>
+        <div className=''>
             <div className='container mx-auto py-3'>
-                <div className="flex justify-between">
-                    <div className="logo-holder"><img src="" alt="" /><h3 className='text-3xl font-bold'>Hasan</h3></div>
+                <div className="flex justify-between items-center">
+                    <div className='nav'>
+                        <Link to='/home'>Home</Link>
+                        <Link to='/portfolio'>Portfolio</Link>
+                        <Link to='/portfolio'>Skils</Link>
+                        <Link to='/portfolio'>Portfolio</Link>
+                    </div>
+                    <div className="logo-holder w-[120px]"><img src={logo} alt="" /></div>
                     <div>
-                        <h2 className='text-3xl font-bold'>Menu</h2>
+                       <button className='btn'>Download Resume</button>
                     </div>
                 </div>
             </div>
