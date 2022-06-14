aa60529fab6cd6f7371770453b34ef5c89688b3c
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-06-14 22:29:24 +0600

    removing contact form

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -15,7 +15,7 @@ const Home = () => {
             <hr className='py-10 w-4/5 mx-auto' />
             <MyProject></MyProject>
             <h2 className='text-center my-3 md:mt-[150px] font-samibold text-5xl'>Contact</h2>
-            <Contact></Contact>
+            {/* <Contact></Contact> */}
 
         </div>
     );
