cf5e7f90a9c52b7a1fb58564ba3e5d20b748d5d1
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-06-16 23:58:35 +0600

    adding photo holder

--- src/component/Home/ProfilePic.js
+++ src/component/Home/ProfilePic.js
@@ -1,14 +1,13 @@
-import React from 'react';
-
+import React from "react";
 
 const ProfilePic = () => {
-    return (
-        <div className='border br-15'>
-            <div className="pic-holder flex flex-col justify-center items-center">
-                <div className='pro-pic'></div>
-            </div>
-        </div>
-    );
+  return (
+    <div className="border br-15 profile-card flex flex-col justify-center items-center">
+      <div className="pic-holder">
+        <div className="pro-pic"></div>
+      </div>
+    </div>
+  );
 };
 
 export default ProfilePic;
