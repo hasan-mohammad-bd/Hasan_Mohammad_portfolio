423f7bcd4c8652f35a4d3d3cc35b242e26cb41f3
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-06-17 00:59:18 +0600

    addig background with blour effect

--- src/component/Home/ProfilePic.js
+++ src/component/Home/ProfilePic.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const ProfilePic = () => {
   return (
-    <div className="border br-15 profile-card flex flex-col justify-center items-center">
+    <div className="br-15 profile-card flex flex-col justify-center items-center">
       <div className="pic-holder">
         <div className="pro-pic"></div>
       </div>
