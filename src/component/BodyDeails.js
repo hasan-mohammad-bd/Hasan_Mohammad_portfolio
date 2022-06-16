0674240be7be74a74a5de0b2947c7a6ffa9f3e74
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-06-16 22:37:58 +0600

    adding prifile and body details

---
+++ src/component/BodyDeails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BodyDeails = () => {
+    return (
+        <div>
+            <h3>This is body details section</h3>
+        </div>
+    );
+};
+
+export default BodyDeails;
--- src/component/Home/Body.js
+++ src/component/Home/Body.js
@@ -1,10 +1,12 @@
 import React from "react";
+import BodyDeails from "../BodyDeails";
+import Profile from "./Profile";
 
 const Body = () => {
   return (
     <div className="flex body">
-      <div className="profile w-1/4 border br-15">tis is the test</div>
-      <div className="body w-3/4 border br-15"> this is the text and its a body</div>
+      <div className="profile w-1/4 border br-15"><Profile></Profile></div>
+      <div className="body w-3/4 border br-15"><BodyDeails></BodyDeails></div>
     </div>
   );
 };
