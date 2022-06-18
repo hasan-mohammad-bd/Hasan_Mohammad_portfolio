1cdde42f307633e734b0d1d53e17a035635e972d
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-06-18 22:58:57 +0600

    removing the skill

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -3,16 +3,15 @@ import Body from './Body';
 
 
 
-import MyProject from './MyProject';
+
 import ShowCase from './ShowCase';
-import Skill from './Skill';
+
 
 const Home = () => {
     return (
         <div className="container mx-auto">
             <ShowCase></ShowCase>
             <Body></Body>
-            <Skill></Skill>
             <hr className='py-10 w-4/5 mx-auto' />
             <h2 className='text-center my-3 md:mt-[150px] font-samibold text-5xl'>Contact</h2>
             {/* <Contact></Contact> */}
