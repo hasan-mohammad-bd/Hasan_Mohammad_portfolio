fa01c159689ddc325777e15568b985dab74f356e
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-06-18 22:57:38 +0600

    removing the project from home page

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -14,7 +14,6 @@ const Home = () => {
             <Body></Body>
             <Skill></Skill>
             <hr className='py-10 w-4/5 mx-auto' />
-            <MyProject></MyProject>
             <h2 className='text-center my-3 md:mt-[150px] font-samibold text-5xl'>Contact</h2>
             {/* <Contact></Contact> */}
 
