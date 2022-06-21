d21dbcf077cd34440f4e250558a1a55a62d4fd0e
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-06-21 21:53:23 +0600

    adding margin

--- src/component/Home/Body.js
+++ src/component/Home/Body.js
@@ -5,8 +5,8 @@ import Profile from "./Profile";
 const Body = () => {
   return (
     <div className="flex flex-col md:flex-row body">
-      <div className="profile md:w-1/4 border br-15"><Profile></Profile></div>
-      <div className="body md:w-3/4 border br-15 bb"><BodyDeails></BodyDeails></div>
+      <div className="profile md:w-1/4 br-15"><Profile></Profile></div>
+      <div className="body md:w-3/4 ml-3 br-15 bb"><BodyDeails></BodyDeails></div>
     </div>
   );
 };
