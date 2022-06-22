5c2aefe1b6c031d644af7ce185ebd5b3342226cf
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-06-22 23:44:12 +0600

    adding cover letter component

---
+++ src/component/CoverLetter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CoverLetter = () => {
+    return (
+        <div>
+            <h2>This is cover latter</h2>
+        </div>
+    );
+};
+
+export default CoverLetter;
