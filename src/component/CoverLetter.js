f568ecc59ddb33146d99ffd5f17d7bd5ea2a5f17
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-06-23 18:09:15 +0600

    reving soft skill

--- src/component/CoverLetter.js
+++ src/component/CoverLetter.js
@@ -3,8 +3,7 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 const CoverLetter = () => {
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const { register, handleSubmit,reset } = useForm();
   const [companyName, setCompanyName] = useState("______");
   const [position, setPosition] = useState("______");
   const [jobPostPlatform, setJobPostPlatform] = useState("______");
@@ -14,37 +13,90 @@ const CoverLetter = () => {
   const [side, setSide] = useState("______");
   const [productToMake, setProductMake] = useState("______");
 
-
+  const onSubmit = data => {
+    setCompanyName(data.companyName);
+    setPosition(data.position);
+    setJobPostPlatform(data.platform);
+    setSide(data.side);
+    setSkill(data.skill);
+    setProductMake(data.product);
+    setComapnayAchivement(data.achievements)
+    // setSoftSkill(data.softSkill)
+    reset()
+  }
 
   return (
-    <div className=" h-screen bg-white flex justify-center items-center flex-col">
+    <div className=" h-screen bg-white flex justify-center items-center flex-col container mx-auto">
       <div>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <input {...register("firstName")} />
-          <select {...register("gender")}>
-            <option value="female">female</option>
-            <option value="male">male</option>
-            <option value="other">other</option>
-          </select>
-          <input type="submit" />
+        <input
+            className="input w-full max-w-xs input-bordered mb-2 input-success"
+            type="text"
+            placeholder="Company Name"
+            {...register("companyName", { required: false })}
+          />
+        <input
+            className="input w-full max-w-xs input-bordered mb-2 input-success"
+            type="text"
+            placeholder="Position"
+            {...register("position", { required: false })}
+          />
+        <input
+            className="input w-full max-w-xs input-bordered mb-2 input-success"
+            type="text"
+            placeholder="Job Posting platform"
+            {...register("platform", { required: false })}
+          />
+        <input
+            className="input w-full max-w-xs input-bordered mb-2 input-success"
+            type="text"
+            placeholder="Full-stack/Front-end"
+            {...register("side", { required: false })}
+          />
+        <input
+            className="input w-full max-w-xs input-bordered mb-2 input-success"
+            type="text"
+            placeholder="Required Skill"
+            {...register("skill", { required: false })}
+          />
+        <input
+            className="input w-full max-w-xs input-bordered mb-2 input-success"
+            type="text"
+            placeholder="Product to make"
+            {...register("product", { required: false })}
+          />
+        <input
+            className="input w-full max-w-xs input-bordered mb-2 input-success"
+            type="text"
+            placeholder="Company Achievements"
+            {...register("achievements", { required: false })}
+          />
+        {/* <input
+            className="input w-full max-w-xs input-bordered mb-2 input-success"
+            type="text"
+            placeholder="Soft Skill"
+            {...register("softSkill", { required: false })}
+          /> */}
+                    <input
+            className="btn btn-accent input w-full max-w-xs"
+            type="submit"
+          />
         </form>
       </div>
       <h2 className="text-3xl font-bold my-5">Cover Letter</h2>
       <p>
-        I am pleased to be applying for the {position} at {companyName} as
+        I am pleased to be applying for the {position} position at {companyName} as
         posted on {jobPostPlatform}.
         <br />
         <br />
-        My extensive experience in {side} development specializing in {skill}{" "}
+        My extensive experience in {side} development specializing in {skill},
         and building {productToMake} that excellently fit with the position
         requirement as advertised.
         <br />
         <br />
-        Your {companyAchivement} really motivate me and feel my competence in{" "}
-        {softSkill} would be a great benefit to the {companyName}.
+        Your {companyAchivement} really motivate me and I feel my competence in teamwork, technical communication, and problem-solving would be a great benefit to the {companyName}.
         <br />
-        <br />I am grateful to you for the time in reviewing my application.
-        Please find my resume attached. I look forward to hearing from you soon.
+        <br />I would be grateful to you for the time to review my application. Please find my resume attached. I look forward to hearing from you soon.
       </p>
     </div>
   );
