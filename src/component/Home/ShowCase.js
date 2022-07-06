2bde7dbe135979c030d5015859993e84a3723652
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-07-06 22:53:40 +0600

    changing background color

--- src/component/Home/ShowCase.js
+++ src/component/Home/ShowCase.js
@@ -1,19 +1,19 @@
 import Spline from "@splinetool/react-spline";
 import React from "react";
 
-
-
 const ShowCase = () => {
   return (
-    <div className="show-case flex">
-      <div>
-        <h1>I'm a professional web developer </h1>
-      </div>
-
-        <div className="spline-container">
-        <Spline scene="https://prod.spline.design/GigmP5SlMGNsav1a/scene.splinecode" />
+    <div className="show-case flex justify-between">
+      <div className="flex justify-center items-center w-100">
+        <div>
+          <h2>Hello,</h2>
+          <h2 className="text-7xl">I am Hasan.</h2>
+          <h3>MERN Stack Developer</h3>
         </div>
-
+      </div>
+      <div className="spline-container">
+        <Spline scene="https://prod.spline.design/GigmP5SlMGNsav1a/scene.splinecode" />
+      </div>
     </div>
   );
 };
