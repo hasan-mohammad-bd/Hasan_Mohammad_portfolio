62ded08f59923153c9602d699fa3f4fca75377d4
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-07-11 20:51:41 +0600

    adding text to showcase

--- src/component/Home/ShowCase.js
+++ src/component/Home/ShowCase.js
@@ -3,12 +3,22 @@ import React from "react";
 
 const ShowCase = () => {
   return (
-    <div className="show-case text-white flex">
-      <div className="w-100 static h-screen flex items-center w-[600px] justify-center">
-        <div className="w-100">
-          <h5>Hello,</h5>
-          <h5 className="text-white">I am Hasan.</h5>
-          <h5>A Full-Stack Developer</h5>
+    <div className="show-case text-white flex flex-col-reverse md:flex-row justify-between container mx-auto">
+      <div className="w-100 h-screen flex items-center md:w-[550px] justify-center z-10">
+        <div className="">
+          <h6 className="hello md:text-[60px] text-3xl text-[#ffe600] static mr-[100px]">
+            Hello,
+          </h6>
+          <h6 className="text-white hasan text-5xl md:text-[90px] mt-6 md:mt-12 md:w-[550px]">
+            I am Hasan.
+          </h6>
+          <h6 className="full-stack md:text-[32px] text-3xl text-[#59c378] mt-3 md:mt-8">
+            A Full-Stack Web Developer
+          </h6>
+          <h6 className="text-white text-xl text-left">
+            A motivated individual with in-depth knowledge of full-stack web development
+            tools and technology.
+          </h6>
         </div>
       </div>
       <div className="spline-container">
