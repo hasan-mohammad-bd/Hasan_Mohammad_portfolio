8243ce6e0c3c52f4616546d0b126bd44bae99800
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-07-11 18:49:03 +0600

    adding introduction

--- src/component/Home/ShowCase.js
+++ src/component/Home/ShowCase.js
@@ -3,16 +3,19 @@ import React from "react";
 
 const ShowCase = () => {
   return (
-    <div className="show-case flex justify-between">
-      <div className="flex justify-center items-center w-100">
-        <div>
-          <h2>Hello,</h2>
-          <h2 className="text-7xl">I am Hasan.</h2>
-          <h3>MERN Stack Developer</h3>
+    <div className="show-case text-white flex">
+      <div className="w-100 static h-screen flex items-center w-[600px] justify-center">
+        <div className="w-100">
+          <h5>Hello,</h5>
+          <h5 className="text-white">I am Hasan.</h5>
+          <h5>A Full-Stack Developer</h5>
         </div>
       </div>
       <div className="spline-container">
-        <Spline scene="https://prod.spline.design/GigmP5SlMGNsav1a/scene.splinecode" />
+        <Spline
+          className=""
+          scene="https://prod.spline.design/GigmP5SlMGNsav1a/scene.splinecode"
+        />
       </div>
     </div>
   );
