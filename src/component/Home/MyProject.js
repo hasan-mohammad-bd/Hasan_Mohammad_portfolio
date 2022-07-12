ce6ef9073a1cb4908f8ae8d8e4e7e7a790a8dc3c
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-07-12 18:30:00 +0600

    adding glowing text

--- src/component/Home/MyProject.js
+++ src/component/Home/MyProject.js
@@ -1,32 +1,42 @@
-import React, { useEffect, useState } from 'react';
-import ProjectDetails from './ProjectDetails';
-import ProjectNew from './ProjectNew';
-import arrow3 from '../../img/Asset 2@4x.png';
+import React, { useEffect, useState } from "react";
+import ProjectDetails from "./ProjectDetails";
+import ProjectNew from "./ProjectNew";
+import arrow3 from "../../img/Asset 2@4x.png";
 
 const MyProject = () => {
-    const [projects, setProjects] = useState([])
-    console.log(projects);
+  const [projects, setProjects] = useState([]);
+  console.log(projects);
 
-    useEffect(()=>{
-        fetch('https://radiant-lake-65921.herokuapp.com/project')
-        .then(res => res.json())
-        .then(data => setProjects(data))
-    },[])
-    
-    return (
-        <div id='project1' className='mx-auto container text-center'>
-           <h2 className='my-3 text-white projectText text-5xl text-[#ffe600]'>Projects.</h2>
-           <div className='w-[100px] mx-auto relative'>
-           <img className='absolute left-10' src={arrow3} alt="" />
-           <img className='absolute arrow-mirror top-16 right-20' src={arrow3} alt="" />
-           </div>
-           <div className='grid grid-cols-1 md:grid-cols-3 gap-5'>
-            {
-                projects.map(project => <ProjectNew project={project} key={project._id}></ProjectNew>)
-            }
-           </div>
-        </div>
-    );
+  useEffect(() => {
+    fetch("https://radiant-lake-65921.herokuapp.com/project")
+      .then((res) => res.json())
+      .then((data) => setProjects(data));
+  }, []);
+
+  return (
+    <div id="project1" className="mx-auto container text-center">
+      <div class="sign">
+        <span class="sign__word">my</span>
+        <span class="sign__word">recent</span>
+      </div>
+      <h2 className="my-3 text-white projectText text-5xl text-[#ffe600]">
+        Projects.
+      </h2>
+      <div className="w-[100px] mx-auto relative">
+        <img className="absolute left-10" src={arrow3} alt="" />
+        <img
+          className="absolute arrow-mirror top-16 right-20"
+          src={arrow3}
+          alt=""
+        />
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-5 .span">
+        {projects.map((project) => (
+          <ProjectNew project={project} key={project._id}></ProjectNew>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MyProject;
