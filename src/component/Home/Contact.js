99637d85837f30e7ff30584a4f19f1081a65905e
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-07-17 18:40:17 +0600

    adding favicon

--- src/component/Home/Contact.js
+++ src/component/Home/Contact.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import contact from '../../img/contact4.png';
+import React from "react";
+import contact from "../../img/contact4.png";
 
 const Contact = () => {
-    return (
-        <div id='contact' className='form1 relative mt-[200px]'>
-            <img className="absolute z-index" src={contact} alt=""/>
-            <div class="shade">
-    <div class="blackboard">
-        <div class="form">
+  return (
+    <div id="contact" className="form1 relative mt-[200px]">
+      <img className="absolute z-index" src={contact} alt="" />
+      <div class="shade">
+        <div class="blackboard">
+          <div class="form">
             <p>
-                <label>Name: </label>
-                <input className="" type="text" />
+              <label>Name: </label>
+              <input className="" type="text" />
             </p>
             <p>
-                <label>Email: </label>
-                <input type="text" />
+              <label>Email: </label>
+              <input type="text" />
             </p>
             <p>
-                <label>Message: </label>
-                <textarea className='border-b-2'></textarea>
+              <label>Message: </label>
+              <textarea className="border-b-2"></textarea>
             </p>
             <p class="wipeout">
-                <input className='btn' type="submit" value="Send" />
+              <input className="btn" type="submit" value="Send" />
             </p>
+          </div>
         </div>
+      </div>
     </div>
-</div>
-        </div>
-    );
+  );
 };
 
 export default Contact;
